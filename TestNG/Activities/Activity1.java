2897abff9aa10b8f810feff6fb044c92d2dd9ef8
madhuri3499 <madhuri3499@gmail.com>
2022-05-11 07:29:40 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity1.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Activity1 {
+    WebDriver driver;
+    @BeforeMethod
+    public void beforeMethod(){
+        driver = new ChromeDriver();
+        driver.get("https://www.training-support.net");
+    }
+
+    @Test
+    public void test(){
+        String title = driver.getTitle();
+        System.out.println("Page title is"+title);
+        Assert.assertEquals("Training Support",title);
+        //Find the clickable link on the page and click it
+        driver.findElement(By.id("about-link")).click();
+
+        //Print title of new page
+        System.out.println("New page title is: " + driver.getTitle());
+
+        Assert.assertEquals(driver.getTitle(), "About Training Support");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        driver.close();
+    }
+}
