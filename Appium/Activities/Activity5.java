c5ec42ab98c5dcfd9ea042d7b2d88db9646a4b0e
madhuri3499 <madhuri3499@gmail.com>
2022-05-13 15:38:36 +0530

    Delete Activity5.java

--- Appium/Activities/Activity5.java
+++
@@ -1,9 +0,0 @@
-package activities;
-
-public class Activity5 {
-    public static  void main(String[] args){
-        MyBook newNovel = new MyBook();
-        newNovel.setTitle("The Secret");
-        System.out.println(newNovel.getTitle());
-    }
-}
